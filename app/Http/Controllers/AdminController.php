6c20195c604720892e902aa5ac824b511fa3c29a
Anders Wiberg Olsen <awo@Anderss-Air.P-2601HNU-F1>
2016-08-05 16:11:41 +0200

    Add authentication to AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 class AdminController extends Controller
@@ -21,6 +21,15 @@ class AdminController extends Controller
     public function manageCompany($company_id)
     {
       $company = \App\Company::where('id', $company_id)->firstOrFail();
+      //dd($tmp = Auth::user()->companies->get("name")->toArray);
+      //echo in_array($company->name, $tmp);
+      return view('admin.manageCompany')->with('company', $company);
+      //dd(Auth::user()->companies);
+      /*if (in_array($company, Auth::user()->companies)) {
+        return view('admin.manageCompany');
+      } else {
+        return view('admin.401');
+      }*/
       dd($company);
     }
 }
---
+++ resources/views/admin/401.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <h1>401 - Not Autherized!</h1>
+  </div>
+</div>
+@endsection
